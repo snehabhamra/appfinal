9414eb1b2c3b92eb77ec85e3b45bee74283c89f7
Sneha <81133168+snehabhamra@users.noreply.github.com>
2021-08-14 17:41:35 +1000

    added further changes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,4 @@
 class Profile < ApplicationRecord
+    has_many: products_to_purchase, class_name: 'Product', foreign_key: "buyer_id"
+    has_many: products_to_sell, class_name: 'Product', foreign_key: "seller_id"
 end
