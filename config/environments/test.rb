faefae1d8c8c4594244ebd38fa261dbd3a76f2d7
Sneha <81133168+snehabhamra@users.noreply.github.com>
2021-08-14 17:09:45 +1000

    added devise views and added some basic buttons to navbar

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
